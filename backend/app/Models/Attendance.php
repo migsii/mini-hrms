3585114171e292e2a2bfdf0446abc2a1ada30a4d
migsii <migsiimanalastas@gmail.com>
2026-06-15 22:31:44 +0800

    feat: update models

--- backend/app/Models/Attendance.php
+++ backend/app/Models/Attendance.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public function salary()
+    {
+        return $this->hasOne(Salary::class);
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function payrolls()
+    {
+        return $this->hasMany(Payroll::class);
+    }
 }
